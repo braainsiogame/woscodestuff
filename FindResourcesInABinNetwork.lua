a74c74011c98e2fe18441e934142bc883967d10d
iogame <59963353+braainsiogame@users.noreply.github.com>
2023-12-06 22:40:41 +0700

    Update FindResourcesInABinNetwork.lua

--- FindResourcesInABinNetwork.lua
+++ FindResourcesInABinNetwork.lua
@@ -1,15 +1,18 @@
 local Disk = GetPartFromPort(1, "Disk")
 local Bins = GetPartsFromPort(1, "Bin")
 Resources = {}
-for _, bin in Bins do 
-if table.find(Resources, bin) == nil then 
-table.insert(Resources, bin)
+Disk:ClearDisk()
+for _, bin in Bins do
+if table.find(Resources, bin:GetResource()) == nil and bin:GetResource() ~= nil then 
+table.insert(Resources, bin:GetResource())
 end
-Disk:Write("Resources", resources)
-if Disk:Read(bin:GetResource()) == nil then 
-Disk:Write(bin:getResource(), bin:GetAmount())
-else 
-Disk:Write(bin:getResource(), Disk:Read(bin:GetResource()) + bin:GetAmount()
-end
+Disk:Write("Resources", Resources)
+if Disk:Read(bin:GetResource()) == nil and bin:GetResource() ~= nil then 
+Disk:Write(bin:GetResource(), bin:GetAmount())
+else
+Disk:Write(bin:GetResource(), Disk:Read(bin:GetResource()) + bin:GetAmount())
 end 
+end
+for i, resource in Disk:Read("Resources") do 
+print("There are " .. Disk:Read(resource) .. " in " .. resource)
 end
